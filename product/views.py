4febeb50addc0ee87e307c99b8b0e0bb68f917c1
LookerKy <nagaraz07@gmail.com>
2021-03-29 18:25:24 +0900

    add admin rule decorator

--- product/views.py
+++ product/views.py
@@ -4,7 +4,7 @@ from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
-from users.decorators import login_required
+from users.decorators import admin_required
 
 
 # Create your views here.
@@ -14,7 +14,7 @@ class ProductListView(ListView):
     context_object_name = 'product_list'
 
 
-@method_decorator(login_required, name='dispatch')
+@method_decorator(admin_required, name='dispatch')
 class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
--- users/models.py
+++ users/models.py
@@ -4,8 +4,10 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     email = models.EmailField(verbose_name="이메일")
-    password = models.CharField(max_length=64, verbose_name="비밀번호")
+    password = models.CharField(max_length=128, verbose_name="비밀번호")
     register_date = models.DateTimeField(auto_now_add=True, verbose_name="가입일")
+    rule = models.CharField(max_length=8, verbose_name='권한',
+                            choices=(('admin', 'admin'), ('user', 'user')))
 
     def __str__(self):
         return self.email
